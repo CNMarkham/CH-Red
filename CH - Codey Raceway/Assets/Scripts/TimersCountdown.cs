72f24d61234cf9009d1b43c459058a088955c323
I got dismembled!!! <132835392+CNMarkham@users.noreply.github.com>
2026-03-22 15:00:40 -0400

    119

--- CH - Codey Raceway/Assets/Scripts/TimersCountdown.cs
+++ CH - Codey Raceway/Assets/Scripts/TimersCountdown.cs
@@ -2,37 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TimersCountdown : MonoBehaviour
 {
     public Text lapTime;
     public Text startCountdown;
+    public Text LoseText;
 
     public float totalLapTime;
     public float totalCountdownTime;
 
+    public void Start()
+    {
+        LoseText.enabled = false;
+    }
     public void laptime()
     {
         
         lapTime.text = Mathf.Round(totalLapTime).ToString();
     }
 
+    public void ReloadScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
     void Update()
     {
-        
         totalCountdownTime -= Time.deltaTime;
-        totalLapTime -= Time.deltaTime;
 
-        Invoke(nameof(laptime), 3.0f);
+        startCountdown.text = Mathf.Round(totalCountdownTime).ToString();
 
         if (totalCountdownTime <= 0.01f)
         {
-            startCountdown.text = Mathf.Round(totalCountdownTime).ToString();
-            Destroy(startCountdown);
+            totalLapTime -= Time.deltaTime;
+            startCountdown.enabled = false;
+            laptime();
         }
-        else
+        if (totalLapTime <= 0)
         {
-            
+            print("you lost");
+            lapTime.enabled = false;
+            LoseText.enabled = true;
+            Invoke("ReloadScene", 3);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            totalLapTime -= 10;
         }
     }
 }
--- CH - Codey Raceway/Assets/Scripts/TriggerFinishLine.cs
+++ CH - Codey Raceway/Assets/Scripts/TriggerFinishLine.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TiggerFinishLine : MonoBehaviour
 {
+    public Text winText;
     public CheckpointCounter checkpointTracker;
+    private void Start()
+    {
+        winText.enabled = false;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             if (checkpointTracker.triggeredCheckpoints == checkpointTracker.numberOfCheckpoints)
             {
+                winText.enabled = true;
                 print("You Win");
             }
             else
